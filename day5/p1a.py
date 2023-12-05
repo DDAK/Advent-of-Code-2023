154da92895d83a6a3c42c75902323ebedbd55a34
Dawood <dawood.khan@gmail.com>
2023-12-05 23:36:26 +0400

    working sol commit for days 5

--- day5/p1a.py
+++ day5/p1a.py
@@ -3,23 +3,41 @@ def ranger(Dstart,Sstart, incr, Map = {}):
     for a,b in zip(range(Sstart, Sstart+incr), range(Dstart, Dstart+incr)):
         Map[a] = b
 
-
-if __name__ == '__main__':
-    data = open("/Users/ddak/work/Advent-of-Code-2023/day5/input1.txt", encoding="UTF-8").read().split('\n')
+def build_map(data):
     maps = {}
     itter = data.__iter__()
     line = next(itter)
     seeds = line.split(':')[1].split()
-    eof = 0
     line = next(itter, None)
-    while(line):
+    line = next(itter, None)
+    loc_iter = []
+    while(line is not None):
         l = line.split(':')[0]
         nl = next(itter)
         array = {}
-        while(nl):
+        while(nl is not None):
             Dstart, Sstart, incr = nl.split()
             maps[l] = ranger(int(Dstart), int(Sstart), int(incr), array)
             nl = next(itter, None)
+            if nl == '':
+                nl = None
         maps[l] = array
+        loc_iter.append(l)
         line = next(itter, None)
+    return maps, seeds, loc_iter
+
+def traverse_map(seed, maps, locs):
+    next_value = int(seed)
+    for v in locs:
+        next_value = maps[v].get(next_value,next_value)
+    return next_value
+
+if __name__ == '__main__':
+    data = open("/Users/ddak/work/Advent-of-Code-2023/day5/input.txt", encoding="UTF-8").read().split('\n')
+    maps, seeds, locs = build_map(data)
+    locations = []
+    for seed in seeds:
+        locations.append(traverse_map(seed, maps, locs))
+   
+    print(min(locations))
                             
