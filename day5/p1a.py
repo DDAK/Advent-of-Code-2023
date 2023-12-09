0b130646756cb7b0a48e3247aa98ab1d204bd809
Dawood <dawood.khan@gmail.com>
2023-12-09 11:41:44 +0400

    tuple based intermediate code

--- day5/p1a.py
+++ day5/p1a.py
@@ -1,43 +1,51 @@
+# import numpy as np
 
-def ranger(Dstart,Sstart, incr, Map = {}):
-    for a,b in zip(range(Sstart, Sstart+incr), range(Dstart, Dstart+incr)):
-        Map[a] = b
+# def ranger(Dstart, Sstart, incr):
+#     return dict(zip(np.arange(Sstart, Sstart + incr), np.arange(Dstart, Dstart + incr)))
+
+
+# def ranger(Dstart,Sstart, incr):
+#     return {a: b for a, b in zip(range(Sstart, Sstart + incr), range(Dstart, Dstart + incr))}
+
+def ranger(Dstart,Sstart, incr):
+    return {(Sstart,Sstart+incr):(Dstart, Dstart + incr)}
+
+
 
 def build_map(data):
     maps = {}
-    itter = data.__iter__()
-    line = next(itter)
-    seeds = line.split(':')[1].split()
-    line = next(itter, None)
-    line = next(itter, None)
+    itter = iter(data)
+    seeds = next(itter).split(':')[1].split()
+    next(itter, None)
+    # next(itter, None)
     loc_iter = []
-    while(line is not None):
+
+    line = next(itter,None)
+    while line is not None:
         l = line.split(':')[0]
         nl = next(itter)
         array = {}
-        while(nl is not None):
-            Dstart, Sstart, incr = nl.split()
-            maps[l] = ranger(int(Dstart), int(Sstart), int(incr), array)
+
+        while nl and nl.strip():
+            Dstart, Sstart, incr = map(int, nl.split())
+            array.update(ranger(Dstart, Sstart, incr))
             nl = next(itter, None)
-            if nl == '':
-                nl = None
+
         maps[l] = array
         loc_iter.append(l)
         line = next(itter, None)
-    return maps, seeds, loc_iter
+    return seeds, loc_iter, maps
 
-def traverse_map(seed, maps, locs):
+def traverse_map(seed, locs, maps):
     next_value = int(seed)
     for v in locs:
         next_value = maps[v].get(next_value,next_value)
     return next_value
 
 if __name__ == '__main__':
-    data = open("/Users/ddak/work/Advent-of-Code-2023/day5/input.txt", encoding="UTF-8").read().split('\n')
-    maps, seeds, locs = build_map(data)
-    locations = []
-    for seed in seeds:
-        locations.append(traverse_map(seed, maps, locs))
-   
+    data = open("/Users/ddak/work/Advent-of-Code-2023/day5/input1.txt", encoding="UTF-8").read().split('\n')
+    seeds, locs, maps = build_map(data)
+    locations = [traverse_map(seed, locs, maps) for seed in seeds]
+
     print(min(locations))
                             
